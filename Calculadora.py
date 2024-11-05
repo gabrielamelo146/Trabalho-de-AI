ca80540c049c72a6a6542e300d26cf8fd124d3c0
gabrielamelo146 <gabrielamelosemedo@gmail.com>
2024-11-05 09:36:18 +0000

    criei um menu de opções

---
+++ Calculadora.py
@@ -0,0 +1,13 @@
+
+def calculadora():
+   
+    #menu de opções
+    while True:
+        print("***********Menu de opções************")
+        print("+ : Adição")
+        print("- : Subtração")
+        print("* : Multiplicção")
+        print("/ : Divisão")
+        print("Saída = s ")
+        print("hist")
+        Operador=input("Escolha uma opção")
