4aa8a1433ef847d536928a8f0ecab0959e12a5a0
gabrielamelo146 <gabrielamelosemedo@gmail.com>
2025-02-04 08:48:11 +0000

    Acrescentei a opção associar fornecedores

--- produto.py
+++ produto.py
@@ -15,3 +15,18 @@ class Produto:
         def from_string(data_str):
             nome, preco,quantidade,tipo,fornecedores = data_str.strip().split(",")
             return Produto(nome,float(preco), quantidade, tipo,fornecedores)
+#Associar Fornecedor
+def associar_fornecedor(self, fornecedor):
+        if fornecedor not in self.fornecedores:
+            self.fornecedores.append(fornecedor)
+            print(f"Fornecedor {fornecedor.nome} associado ao produto {self.nome} com sucesso!")
+        else:
+            print("Este fornecedor já está associado a este produto.")
+#Listar fornecedores associados
+    def listar_fornecedores(self):
+        if not self.fornecedores:
+            print("Nenhum fornecedor associado a este produto.")
+        else:
+            print(f"Fornecedores do produto {self.nome}:")
+            for fornecedor in self.fornecedores:
+                print(f"- {fornecedor.nome} ({fornecedor.telefone}, {fornecedor.email})")
